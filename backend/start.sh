88b50d21c0c62f8dc72753250b56bb7aebefddc1
Mina Ibrahiem <minaibrahiem@outlook.com>
2026-04-21 16:00:51 +0400

    Fix auto seed on deploy

--- backend/start.sh
+++ backend/start.sh
@@ -3,9 +3,8 @@ set -e
 
 cd /var/www/html
 
-php artisan config:cache || true
-php artisan route:cache || true
-php artisan view:cache || true
+php artisan config:clear
+php artisan cache:clear
 
 php artisan migrate --force || true
 php artisan db:seed --force || true
