7a2dbc36e0700f2591a97c31f623276239b0f86c
Mina Ibrahiem <minaibrahiem@outlook.com>
2026-04-21 16:07:17 +0400

    Add admin seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,14 +3,18 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call([
-            RolePermissionSeeder::class,
-            ClinicDemoSeeder::class,
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@clinic.test'],
+            [
+                'name' => 'Admin',
+                'password' => bcrypt('password'),
+            ]
+        );
     }
 }
